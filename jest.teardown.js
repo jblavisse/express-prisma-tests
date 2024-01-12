7ad03742df7624073f9f4c20f30b63167eba6793
jblavisse <pro@jblavisse.fr>
2024-01-12 10:44:17 +0100

    Force teardown database

--- jest.teardown.js
+++ jest.teardown.js
@@ -3,4 +3,7 @@ const { execSync } = require("child_process");
 module.exports = async () => {
   // Ici, tu pourrais ajouter une logique pour nettoyer ou remettre à zéro la base de données
   // Par exemple, supprimer toutes les tables ou exécuter un script SQL spécifique
+  execSync("npx prisma migrate reset --preview-feature --force", {
+    stdio: "inherit",
+  });
 };
